cdbc1de544450a3ad0206b811c2a56d61f17cad8
TessShearer <shearer.tess@gmail.com>
2022-03-23 13:22:02 -0600

    fixed player name bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -172,6 +172,17 @@ var shop = function() {
 // player information
 var playerInfo = {
   name: window.prompt("What is your robot's name?"),
+  var getPlayerName = function() {
+    var name = "";
+  
+    while (name === "" || name === null) {
+      name = prompt("What is your robot's name?");
+    }
+    
+  
+    console.log("Your robot's name is " + name);
+    return name;
+  };
   health: 100,
   attack: 10,
   money: 10,
